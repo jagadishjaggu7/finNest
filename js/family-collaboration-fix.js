1ecbf75e1dc072a1945bab3f8cd75ced0efe3684
jagadishjaggu7 <132246274+jagadishjaggu7@users.noreply.github.com>
2026-08-17 09:09:24 +0530

    Prevent family refresh recursion in collaboration fix

--- js/family-collaboration-fix.js
+++ js/family-collaboration-fix.js
@@ -219,7 +219,6 @@
         originalRenderFamilyView = renderFamilyView;
         renderFamilyView = function () {
             originalRenderFamilyView();
-            setTimeout(() => hydrateSharedTransactions().catch(() => {}), 0);
         };
     }
 
